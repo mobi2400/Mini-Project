e3a246583d2e73a209bd8ee46241b95d5cd3d0cd
mobasshir <mobasshirkhan9931@gmail.com>
2025-11-02 11:07:32 +0530

    reset the repo

--- LearningPlatform/prisma/migrations/20251024041424_relation_btw_admin_course_user_upadted/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."Course" DROP CONSTRAINT "Course_authorId_fkey";
-
--- AlterTable
-ALTER TABLE "Course" ALTER COLUMN "authorId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Course" ADD CONSTRAINT "Course_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Admin"("id") ON DELETE SET NULL ON UPDATE CASCADE;
